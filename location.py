9ee45b447c80e0bad00abab96422c6648ea81911
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2024-10-27 18:39:55 +0530

    Update location.py

--- location.py
+++ location.py
@@ -4,6 +4,10 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 
+st.markdown('''<style>div[data-testid="stToolbar"] {
+  visibility: hidden;
+}</style>''',unsafe_allow_html=True)
+
 # JavaScript code to get the user's location and update URL parameters
 js_code = """
 <script>
