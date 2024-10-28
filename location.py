799563f47f885cae6317035a4c854b95546c8a9a
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2024-10-28 16:18:50 +0530

    Update location.py

--- location.py
+++ location.py
@@ -15,6 +15,11 @@ st.markdown('''<style>iframe{
 }
 </style>''',unsafe_allow_html=True)
 
+st.markdown('''<style>body div:nth-child(-n+3) {
+    display: none;
+}
+</style>''',unsafe_allow_html=True)
+
 
 # JavaScript code to get the user's location and update URL parameters
 js_code = """
