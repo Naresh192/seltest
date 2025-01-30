105f53fa80a790818ebffcca1f4f99480e937999
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2025-01-30 17:05:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,22 +22,10 @@ for obj in data['data']:
     
 # JavaScript to get device orientation
 orientation_js = """
-<script>
-function getOrientation() {
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener('deviceorientation', function(event) {
-            var alpha = event.alpha.toFixed(2);
-            var beta = event.beta.toFixed(2);
-            var gamma = event.gamma.toFixed(2);
-            document.getElementById('orientation').innerText = `Alpha: ${alpha}, Beta: ${beta}, Gamma: ${gamma}`;
-        }, false);
-    } else {
-        document.getElementById('orientation').innerText = "Device orientation not supported.";
-    }
-}
-getOrientation();
-// Access the back camera
-async function startCamera() {
+<video id="video" autoplay></video>
+
+    <script>
+        async function startCamera() {
             try {
                 const constraints = {
                     video: {
@@ -52,10 +40,8 @@ async function startCamera() {
             }
         }
 
-startCamera();
-</script>
-<div id="orientation" style="width: 100%; height: 100%;"></div>
-<div id="video" style="width: 100%; height: 100%;"></div>
+        startCamera();
+    </script>
 """
 
 components.html(orientation_js, height=200)
