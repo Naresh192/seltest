875155629578067a26a11aaf13396e1e3d82683a
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2025-02-01 19:15:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -97,6 +97,7 @@ async function getPlanetDistance(planetName) {
 }
 
 function planetToScreenCoords(azimuth, altitude, r, alpha, beta, gamma, fovHorizontal, fovVertical) {
+    console.log('Hello')
     // Convert azimuth and altitude to radians
     const video = document.getElementById('video');
     const windowWidth = video.videoWidth;
