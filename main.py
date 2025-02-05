801b64ab35b94da002a53661f0f3d6b631f0a9be
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2025-02-05 18:57:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ st.title("Orientation")
 orientation_js = """
 <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js"></script>
 <script type='module'>
-import { DeviceOrientationControls } from 'https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/controls/DeviceOrientationControls.js';
 
 function getOrientation() {
     if (window.DeviceOrientationEvent) {
