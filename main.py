0282fc56c9f966994e52cea21713369ab29581bf
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2025-02-05 19:14:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ st.title("Orientation")
 # JavaScript for Device Orientation, Camera Access, and API Call
 orientation_js = """
 <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js"></script>
-<script  type='module'>
-import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js";
+<script  type='module' src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js">
 import { DeviceOrientationControls } from 'https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/controls/DeviceOrientationControls.js';
 
 
